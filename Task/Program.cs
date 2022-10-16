b8c6986d189e713dee87bb108a133fc7f2bb308b
Anna Mmaximenko <any-maksimenk@yandex.ru>
2022-10-16 17:27:56 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,41 @@
+﻿
+//Метод формирующий массив из строк, длина которых <= 3 символа
+string[] GenResultArray(string[] inputArray)
+{
+    string[] ResultArray = new string[inputArray.Length];
+    int count = 0;
+
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+
+            ResultArray[count] = inputArray[i];
+            count++;
+
+
+        }
+    }
+    return ResultArray;
+}
+
+// Вывод результата в консоль
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != null)
+        {
+            Console.Write("[ " + $"{array[i]} " + " ] ");
+        }
+    }
+    Console.WriteLine();
+}
+
+//string[] inputArray = new string[5] { "hello", "23455", "=)", "world", "Kazan" };
+
+string[] inputArray = new string[5] { "1234", "1567", "-2", "computer science", "456" };
+string[] ResultArray = GenResultArray(inputArray);
+
+PrintArray(ResultArray);
